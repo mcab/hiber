55bfeb4dea7d768fcacefc524c07706d9725cb5c
Mark Cabanero <me@markcabanero.com>
2019-03-26 20:08:08 -0400

    Fix API routing (#25)
    In order to prevent drf_yasg auto generating routes for Wagtail URLs, a
    urlconf must be specified when calling get_schema_view. If we generate
    the URL earlier while routing, then all the base URLs generated (for
    Redoc) will be relative to that URL.
    
    To mitigate this, the urlpatterns for the version will generate the API
    for that version within its own urls.py file, instead of being generated
    in the main application. This allows get_schema_view to see that
    urlpattern and base all of the URLs from there, instead of from blank.
    
    This is not intuitive, hence why additional comments were left to
    explain why. Also, we kept the docs underneath the specifically versioned
    one, but don't show up underneath calling the API directly, as to not
    have that route (docs/) in the documentation.

--- hiber/apps/api/views.py
+++ hiber/apps/api/views.py
@@ -1,20 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
 from rest_framework import viewsets
-from rest_framework.response import Response
 from ..bathouse.models import Bat
 from .serializers import BatSerializer, UserSerializer
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'bats':
-        reverse('bat-list', request=request, format=format),
-        'users':
-        reverse('user-list', request=request, format=format),
-    })
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
