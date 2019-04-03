f1726393fed5f444bb619831cdb07838dacfde57
Mark Cabanero <me@markcabanero.com>
2019-04-03 00:19:24 -0400

    Expose models over API (#29)
    This PR allows us to GET / POST data over the API, namely:
        House
        House Environmental Features
        House Physical Features
        Observations
    
    They're located at:
        /api/v1/house
               /house/{pk}/environment
               /house/{pk}/physical
               /house/{pk}/observations
        
    Where {pk} stands for Primary Key; features and observations are
    tied to a specific house, so they belong underneath those API routes.
    
    With this change, certain migrations needed to be deleted in order to
    update some ForeignKeys that were invalid.

--- hiber/apps/api/views.py
+++ hiber/apps/api/views.py
@@ -1,20 +1,61 @@
-from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from ..bathouse.models import Bat
-from .serializers import BatSerializer, UserSerializer
 from rest_framework.views import APIView
-
-
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from ..bathouse.models import (Bat, House, HouseEnvironmentFeatures,
+                               HousePhysicalFeatures, Observation)
+from .serializers import (
+    BatSerializer, HouseSerializer, HouseEnvironmentFeaturesSerializer,
+    HousePhysicalFeaturesSerializer, ObservationSerializer)
 
 
 class BatViewSet(viewsets.ReadOnlyModelViewSet):
+    model = Bat
     queryset = Bat.objects.all()
     serializer_class = BatSerializer
+
+
+class HouseViewSet(viewsets.ModelViewSet):
+    model = House
+    permission_classes = (IsAuthenticated, )
+    queryset = House.objects.all()
+    serializer_class = HouseSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(watcher=self.request.user)
+
+
+class HouseEnvironmentFeaturesViewSet(viewsets.ModelViewSet):
+    model = HouseEnvironmentFeatures
+    permission_classes = (IsAuthenticated, )
+    queryset = HouseEnvironmentFeatures.objects.all()
+    serializer_class = HouseEnvironmentFeaturesSerializer
+
+    def get_queryset(self):
+        return HouseEnvironmentFeatures.objects.filter(
+            house=self.kwargs["house_pk"])
+
+
+class HousePhysicalFeaturesViewSet(viewsets.ModelViewSet):
+    model = HousePhysicalFeatures
+    permission_classes = (IsAuthenticated, )
+    queryset = HousePhysicalFeatures.objects.all()
+    serializer_class = HousePhysicalFeaturesSerializer
+
+    def get_queryset(self):
+        return HousePhysicalFeatures.objects.filter(
+            house=self.kwargs["house_pk"])
+
+
+class ObservationViewSet(viewsets.ModelViewSet):
+    model = Observation
+    permission_classes = (IsAuthenticated, )
+    queryset = Observation.objects.all()
+    serializer_class = ObservationSerializer
+
+    def get_queryset(self):
+        return Observation.objects.filter(house=self.kwargs["house_pk"])
 
 
 class AuthView(APIView):
